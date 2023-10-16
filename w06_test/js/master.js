5ef8b677b7c7e61a57be7b377cd25f7e15eb6d5b
eunjae329 <eunjae329@naver.com>
2023-10-16 15:14:42 +0900

    test

---
+++ w06_test/js/master.js
@@ -0,0 +1,39 @@
+var Body = {
+  setBackgroundColor : function(color){
+    document.querySelector('body').style.backgroundColor = color;
+  },
+  setColor : function(color){
+    document.querySelector('body').style.color = color;
+  }
+}
+var Links = {
+  setColor : function(color){
+    $('a').css('color', color);
+    // var i = 0;
+    // var alist = document.querySelectorAll('a');
+    // while (i < alist.length){
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // } 
+  }
+}
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value=='night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    Links.setColor('darkgreen'); 
+    self.value='day';
+  } else {
+    Body.setBackgroundColor('mediumspringgreen');
+    Body.setColor('black');        
+    Links.setColor('black');
+    self.value='night';
+  }
+}
+var count=0;
+function like() {
+  count +=1;
+  alert("좋아요를 누르셨습니다! 좋아요:"+count);
+}
